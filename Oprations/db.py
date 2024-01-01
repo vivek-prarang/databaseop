0ddfce9decf2346ed4472e511522d49386bfcd8a
saurabh <saurabh.s@prarang.in>
2024-01-01 18:02:15 +0530

    new

--- Oprations/db.py
+++ Oprations/db.py
@@ -2,10 +2,9 @@
 from dbconn import DBCon
 
 class Model:
-    def __init__(self):
-        self.conn = DBCon().connect
-        self.cursor = self.conn.cursor()
     def get_verticals(self):
-        self.cursor.execute("SELECT * FROM verticalsname")
-        verticals = self.cursor.fetchall()
+        conn = DBCon().connect()
+        cursor=conn.cursor()
+        cursor.execute("SELECT * FROM verticalsname")
+        verticals = cursor.fetchall()
         return verticals
--- app.py
+++ app.py
@@ -2,22 +2,24 @@ from asyncio.windows_events import NULL
 from multiprocessing import connection
 from config import Config
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from Oprations.csvscraper import CSV_Scraper 
-from Oprations.db import Model as model
-
+from Oprations.db import Model
 app = Flask(__name__)
 cssscr=CSV_Scraper()
-
-
+model=Model()
 
 @app.route('/', methods=['GET', 'POST'])
 def validate_titles():
     if request.method == 'POST':
         titles, number_of_rows = cssscr.scrape_titles(request)
+        try:
+            verticals=model.get_verticals()
+            print(verticals[0][1])            
+        except:
+            return render_template('validate-datatype.html',message="Verticals List is not available")            
         if number_of_rows is None:
             return render_template('validate-datatype.html',message="Invalid Titles")
-        return render_template('validate-datatype.html', headers=titles, row_count=number_of_rows)
+        return render_template('validate-datatype.html', headers=titles, row_count=number_of_rows,verticals=verticals)
     return render_template('validate-datatype.html')
 
 if __name__ == "__main__":
